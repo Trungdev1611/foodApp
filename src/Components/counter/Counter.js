90520c6b72196b50042a913ad9f16b4b0405f3c9
Trungdev <trungphamk53@gmail.com>
2022-06-01 09:24:21 +0700

    tam xong home

--- src/Components/counter/Counter.js
+++ src/Components/counter/Counter.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react'
 import './counter.scss'
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import Counterbottom from './Counterbottom';
 const Counter = () => {
+
+
     const [videoshow, setVideoshow] = useState(false)
     return (
-        <section className='counter' style={{}}>
-            {videoshow ? <div className="video" onClick={() => setVideoshow(false)}></div> : null}
+        <section className='counter'>
+            {videoshow ?
+                <div>
+                    <div className="modal-video" onClick={() => setVideoshow(false)}></div>
+                    <iframe src="https://www.youtube.com/embed/PxhR2CuVq6Q" title='video'></iframe>
+                </div>
+                : null}
 
             <div className='counter-content'>
                 <div className="counter-heart">
@@ -14,15 +22,16 @@ const Counter = () => {
 
                 </div>
 
-                <div className="counter-effect">
+                <div className="counter-effect" >
                     <span className="top-left"></span>
                     <span className="bottom-left"></span>
                     <span className="top-right"></span>
                     <span className="bottom-right" onClick={() => setVideoshow(true)}>{!videoshow ? <ArrowRightIcon className='arrow-icon' /> :
-                        <iframe src="https://www.youtube.com/embed/PxhR2CuVq6Q" width={622} height={350} title='video'></iframe>
+                        null
                     }</span>
                 </div>
             </div>
+            <Counterbottom />
         </section>
     )
 }
