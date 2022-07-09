b6704458d9ace755d384afd9f2c525c0490fbe9e
Trungdev <trungdev1611@gmail.com>
2022-07-09 10:16:20 +0700

    add Reduxjs-persit persit account logged in

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import store from './redux/store/store'
+import { PersistGate } from 'reduxjs-toolkit-persist/integration/react'
 import { Provider } from 'react-redux';
+import { persistStore } from 'reduxjs-toolkit-persist'
+let persistor = persistStore(store)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
-
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,12 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistReducer } from 'reduxjs-toolkit-persist'
+import storage from 'reduxjs-toolkit-persist/lib/storage' // defaults to localStorage for web
+
 import foodlistReducer from './../SliceReducer/foodlistSlice'
 import AuthSliceReducer from "../SliceReducer/AuthSlice";
-const store = configureStore({
-    reducer: {
-        foodlistReducer,
-        AuthSliceReducer
 
-    }
+const rootReducer = combineReducers({
+    foodlistReducer,
+    AuthSliceReducer
+})
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+    whitelist: ['AuthSliceReducer']
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+
 })
 
 export default store
